924a107068575c96fc55091e05b5e88d6dd8053a
Bijan <mr.bijankumardey@gmail.com>
2023-05-29 18:33:59 +0530

    Second

---
+++ EvenOdd2.java
@@ -0,0 +1,21 @@
+class EvenOdd2
+{
+	static int num(int no1 ,int no2)
+	{
+		for (int i = no1;i<=no2 ;i++ )
+		{
+			if (i%2==0)
+			{
+				return i;
+			}else 
+			{
+				return i;
+			}
+		}
+		return int i;
+	}
+	public static void main(String[] args) 
+	{
+		num(1,10);
+	}
+}
